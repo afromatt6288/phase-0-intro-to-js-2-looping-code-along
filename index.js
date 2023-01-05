dee29e4fbbe69447191b4a30546654bb6788aa77
Matthew Clark <afromatt6288@hotmail.com>
2023-01-05 16:32:28 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+    let thankYouMessage = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessage.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouMessage
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+    return number
+}
+countDown(number)
